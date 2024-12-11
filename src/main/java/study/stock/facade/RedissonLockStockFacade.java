b3be81732c350d75ace441f7132f1391b9537408
naminsu <naminsoo1020@naver.com>
2024-12-12 00:54:08 +0900

    [feat] redis 분산락 Redisson

---
+++ src/main/java/study/stock/facade/RedissonLockStockFacade.java
@@ -0,0 +1,40 @@
+package study.stock.facade;
+
+
+import java.util.concurrent.TimeUnit;
+import lombok.RequiredArgsConstructor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Component;
+import study.stock.service.StockService;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class RedissonLockStockFacade {
+
+    private final RedissonClient redissonClient;
+    private final StockService stockService;
+
+    public void decrease(Long id, Long quantity) {
+        RLock lock = redissonClient.getLock(id.toString());
+
+        try {
+            boolean available = lock.tryLock(10, 1, TimeUnit.SECONDS);
+
+            if(!available) {
+                System.out.println("Lock 획득 실패");
+                log.info("lock 획득 실패");
+                return;
+            }
+
+            stockService.decrease(id, quantity);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
+        }
+    }
+}
