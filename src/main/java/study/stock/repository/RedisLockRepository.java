d53a95761b4353cf6bc7985bd988712559774c6e
naminsu <naminsoo1020@naver.com>
2024-12-12 00:25:22 +0900

    [feat] redis 분산락 Lettuce

---
+++ src/main/java/study/stock/repository/RedisLockRepository.java
@@ -0,0 +1,29 @@
+package study.stock.repository;
+
+
+import java.time.Duration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RedisLockRepository  {
+
+     private final RedisTemplate<String, String> redisTemplate;
+
+     public boolean lock(Long key) {
+         return redisTemplate
+                 .opsForValue().
+                 setIfAbsent(generateKey(key),"lock", Duration.ofMillis(3_000));
+     }
+
+     public boolean unlock(Long key) {
+         return redisTemplate
+                 .delete(generateKey(key));
+     }
+
+     private String generateKey(Long key) {
+         return key.toString();
+     }
+}
