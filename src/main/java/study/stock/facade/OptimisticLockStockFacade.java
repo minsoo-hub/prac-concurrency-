e73501b293fa206eee4590cb0e39fe4230a84958
naminsu <naminsoo1020@naver.com>
2024-12-11 23:56:23 +0900

    [feat] 낙관적 락

---
+++ src/main/java/study/stock/facade/OptimisticLockStockFacade.java
@@ -0,0 +1,23 @@
+package study.stock.facade;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import study.stock.service.OptimisticLockStockService;
+
+@Component
+@RequiredArgsConstructor
+public class OptimisticLockStockFacade {
+
+    private final OptimisticLockStockService optimisticLockStockService;
+
+    public void decrease(Long id, Long quantity) throws InterruptedException {
+        while (true) {
+            try {
+                optimisticLockStockService.decrease(id, quantity);
+                break;
+            } catch (Exception e) {
+                Thread.sleep(50);
+            }
+        }
+    }
+}
