622e7f0fa5f1fc5a27ae62d1d124573c79a03c92
naminsu <naminsoo1020@naver.com>
2024-12-11 23:26:40 +0900

    [feat] 비관적 락 서비스

--- src/main/java/study/stock/service/PessimisticLockStockService.java
+++ src/main/java/study/stock/service/PessimisticLockStockService.java
@@ -1,4 +1,28 @@
 package study.stock.service;
 
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import study.stock.domain.Stock;
+import study.stock.repository.StockRepository;
+
+@Getter
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class PessimisticLockStockService {
+    
+    private final StockRepository stockRepository;
+
+
+    @Transactional
+    public void decrease(Long id, Long quantity) {
+        Stock stock = stockRepository.findByIdWithPessimisticLock(id).orElseThrow();
+        stock.decrease(quantity);
+        stockRepository.save(stock);
+    }
+
 }
